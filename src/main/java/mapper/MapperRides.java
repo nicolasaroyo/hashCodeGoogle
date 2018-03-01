8d8bde3d3b3b872ff28bda16644d0dddd8ce2d75
naroyo <naroyo@192.168.1.10>
2018-03-01 20:45:42 +0100

    First working version !!!

--- src/main/java/mapper/MapperRides.java
+++ src/main/java/mapper/MapperRides.java
@@ -1,28 +1,27 @@
 package mapper;
 
 import problem.Coordinate;
-import problem.Input;
 import problem.Ride;
 
 public class MapperRides {
 
 	public Ride inputToRide(final String inputLine) {
 		final String[] splittedLine = inputLine.split(" ");
-		
+
 		final Ride ride = new Ride();
 		final Coordinate coordStart = new Coordinate();
 		coordStart.setRow(Integer.valueOf(splittedLine[0]));
 		coordStart.setColumn(Integer.valueOf(splittedLine[1]));
-		
+
 		final Coordinate coordFinish = new Coordinate();
-		coordFinish.setRow(Integer.valueOf(splittedLine[3]));
-		coordFinish.setColumn(Integer.valueOf(splittedLine[4]));
-		
+		coordFinish.setRow(Integer.valueOf(splittedLine[2]));
+		coordFinish.setColumn(Integer.valueOf(splittedLine[3]));
+
 		ride.setStart(coordStart);
 		ride.setFinish(coordFinish);
-		ride.setEarliestStart(Integer.valueOf(splittedLine[5]));
-		ride.setLatestFinish(Integer.valueOf(splittedLine[6]));
-		
+		ride.setEarliestStart(Integer.valueOf(splittedLine[4]));
+		ride.setLatestFinish(Integer.valueOf(splittedLine[5]));
+
 		return ride;
 	}
 }
